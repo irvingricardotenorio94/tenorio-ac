d69d70aedb1a0920dc51988e9369a542f5cd497c
IrvingRicardo <37230247+irvingricardotenorio94@users.noreply.github.com>
2026-01-08 11:38:15 -0600

    Revert base url when hosting with correct domain.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/tenorio-ac/' : '/',
+  base: process.env.GITHUB_PAGES ? '/' : '/',
   plugins: [
     react(),
     tailwindcss(), 
